443f8d23ba186d148ec0e366f23247e64f52d746
Unknown <lucie.lesbats@hotmail.fr>
2018-12-06 11:47:04 +0100

    6 dec v2
    Modifs mise en page générale

--- js/slider.js
+++ js/slider.js
@@ -11,9 +11,9 @@ var slideIndex = 1;
         if (n > x.length) {slideIndex = 1}    
         if (n < 1) {slideIndex = x.length}
         for (i = 0; i < x.length; i++) {
-         x[i].style.display = "none";  
+         x[i].classList.remove("divActive");  
         }
-      x[slideIndex-1].style.display = "block";  
+      x[slideIndex-1].classList.add("divActive");  
     }
 
 
